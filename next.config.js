0e9a61b3759228b95d2962cc17c55a7e3f299124
Anna Romanowicz <anna.romanowicz@wpengine.com>
2020-10-26 11:36:39 +0100

    update

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    MY_KEY: process.env.key6,
+  },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,2 @@
-const Home = () => <h1>Hello {process.env.key6}!!!!!</h1>;
+const Home = () => <h1>Hello {process.env.MY_KEY}!!!!!</h1>;
 export default Home;
